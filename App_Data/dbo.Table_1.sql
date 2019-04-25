0cd221e17672612d94918a4e6930f596ffad44a2
Hazierah97 <43959777+Hazierah97@users.noreply.github.com>
2019-04-25 23:44:04 +0100

    Add database using localdb

---
+++ App_Data/dbo.Table_1.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Country] (
+    [CountryID]   INT           IDENTITY (1, 1) NOT NULL,
+    [CountryName] VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([CountryID] ASC)
+);
