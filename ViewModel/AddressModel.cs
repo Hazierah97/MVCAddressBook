0e7b7959214e729d548d78f43463fbe3f311e072
Hazierah97 <43959777+Hazierah97@users.noreply.github.com>
2019-04-25 23:50:51 +0100

    create AddressModel and Views for cshtml

---
+++ ViewModel/AddressModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAddressBook.ViewModel
+{
+    public class AddressModel
+    {
+        public int AddressID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Postcode { get; set; }
+        public string Town { get; set; }
+        public int CountryID { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
